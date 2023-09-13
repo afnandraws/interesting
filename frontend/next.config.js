0d4329a73f912dd0794566ef3d05d60ee7621cb2
Afnan Khan <40701226+afnandraws@users.noreply.github.com>
2023-09-13 20:08:23 +0100

    adjusting config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: false,
+	output: "export",
+	distDir: "_static",
+	images: {
+		unoptimized: true,
+	},
 };
-
 module.exports = nextConfig;
